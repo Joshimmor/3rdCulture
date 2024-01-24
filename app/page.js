b3dcdd1026fe7da276e28cad53c0b97f2a234b49
Joshua Immordino <joshimmor@gmail.com>
2024-01-23 20:12:38 -0500

    learn somethine new

--- app/page.js
+++ app/page.js
@@ -33,7 +33,7 @@ export default function Home() {
           <div className="flex flex-col justify-center items-center">
               <h1 className="text-slate-400 font-bold text-2xl mb-10">Join the culture</h1>
               <p className="text-center text-slate-400 font-bold text-lg mb-10">
-              Join the 3rd Culture Kids's newsletter to get the latest releases and artist news delivered straight to your inbox.
+              Join the 3rd Culture Kids&apos;s newsletter to get the latest releases and artist news delivered straight to your inbox.
               </p>
               <button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-sm border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Subscribe</button>
           </div>
